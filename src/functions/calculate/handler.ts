73d4f5bed6a83bf17534f2c15f372f6c9b67dc91
kewei <tan.kewei@stengg.com>
2022-02-02 17:47:20 +0800

    init calculate func

--- src/functions/calculate/handler.ts
+++ src/functions/calculate/handler.ts
@@ -4,10 +4,39 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`
-  });
+interface Transaction {
+    transactionId: string,
+    orderId: string,
+    merchantId: string,
+    merchantType: string,
+    value: number
+}
+
+const calculate: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+    const transactions: Array<Transaction> = event.body.transactions;
+    let gross_sales = {}, net_sales = {}, average_order_value = {}
+
+    // Find gross sales value & net sales value per merchant
+    // Gross Sales Value = Grand Total of all sale transactions
+    // Net Sales Value = Gross Sales - sales allowances, returns & discounts
+    transactions.forEach((transaction: Transaction): void => {
+        if (!gross_sales[transaction.merchantId]) {
+            if (transaction.value > 0) gross_sales[transaction.merchantId] = transaction.value;
+            net_sales[transaction.merchantId] = transaction.value;
+        } else {
+            if (transaction.value > 0) gross_sales[transaction.merchantId] = gross_sales[transaction.merchantId] + transaction.value;
+            net_sales[transaction.merchantId] = net_sales[transaction.merchantId] + transaction.value;
+        }
+    })
+
+    // Find average order value per merchant
+    // Average Order = Gross sales / number of orders
+
+    return formatJSONResponse({
+        gross_sales,
+        net_sales,
+        average_order_value
+    });
 };
 
-export const main = middyfy(hello);
+export const main = middyfy(calculate);
