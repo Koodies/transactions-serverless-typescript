ae110b121859528c640aac5e125ec2c60c9213b2
kewei <tan.kewei@stengg.com>
2022-02-02 17:46:47 +0800

    Add Transactions Schema

--- src/functions/calculate/schema.ts
+++ src/functions/calculate/schema.ts
@@ -1,8 +1,19 @@
 export default {
-    type: "object",
-    properties: {
-      name: { type: 'string' }
-    },
-    required: ['name']
-  } as const;
-  
+  type: "object",
+  properties: {
+    transactions: {
+      type: "array",
+      transaction: {
+        type: "object",
+        properties: {
+          transactionId: { type: 'string' },
+          orderId: { type: 'string' },
+          merchantId: { type: 'string' },
+          merchantType: { type: 'string' },
+          value: { type: 'number' }
+        }
+      }
+    }
+  },
+  required: ['transactionId','orderId','merchantId','merchantType','value']
+} as const;
